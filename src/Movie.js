b2dbbc70b6e7d14b92611ab3efa8a4378f78d12c
정주원 <filocd05155@naver.com>
2023-06-29 18:08:16 +0900

    영화 화면 표시

---
+++ src/Movie.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <img src={coverImg} alt="" />
+      <p>{summary}</p>
+      {genres && genres.length > 0 ? (
+        <ul>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
