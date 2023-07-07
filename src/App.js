2e88250e4553e831532f4fe43455e0f67e8e5872
정주원 <filocd05155@naver.com>
2023-07-07 20:11:29 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import ReactDom from "react-dom";
 
 import "./styles.css";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
-    setValue(value);
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 
 function App() {
-  const name = useInput("Mr.");
+  const maxLan = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLan);
   return (
     <div className="App">
       <h1>Hello</h1>
